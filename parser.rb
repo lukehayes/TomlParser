1d761430a248530125a13a6de80779491a037412
Luke Hayes <lukepurplehayes@gmail.com>
2023-01-26 10:06:38 +0000

    Added very basic parser written in ruby.

---
+++ parser.rb
@@ -0,0 +1,47 @@
+
+lines = []
+
+def isalpha(str)
+  return false if str.empty?
+  !str.match(/[^A-Za-z]/)
+end
+
+def isnum(str)
+  return false if str.empty?
+  !str.match(/[^0-9]/)
+end
+
+
+lines = File.readlines("data.toml")
+
+lines.each do |line|
+  line.each_char do |c|
+
+    if isalpha(c)
+      print c
+
+    elsif isnum(c)
+      print c
+
+    else
+      case c
+      when '['
+        print "["
+      when ']'
+        print "]"
+      when '='
+        print "="
+      when '"'
+        print '"'
+      when '.'
+        print '.'
+      when ','
+        print ','
+      else
+      end
+    end
+
+  end
+    puts " "
+end
+
