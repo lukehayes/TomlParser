e090fae6979ca153ab20d35acc4a718ba2bdc6e6
Luke Hayes <lukepurplehayes@gmail.com>
2023-01-27 09:43:01 +0000

    Added toml_print.h helper functions.

---
+++ include/toml_print.h
@@ -0,0 +1,16 @@
+#ifndef TOML_PRINT_H
+#define TOML_PRINT_H
+
+#include <stdio.h>
+
+/**-----------------------------------------------------------------------------
+ = HELPFUL TOML PRINT FUNCTIONS
+-----------------------------------------------------------------------------**/
+
+void PrintString(const char* str)
+{
+    printf("STR: %s\n", str);
+}
+
+
+#endif // TOML_PRINT_H
