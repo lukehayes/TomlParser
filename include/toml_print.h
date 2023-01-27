e982d3ea5ca92ddbf198e43b769c276d2732263e
Luke Hayes <lukepurplehayes@gmail.com>
2023-01-27 10:19:02 +0000

    Added PrintBuffer() helper function

--- include/toml_print.h
+++ include/toml_print.h
@@ -12,5 +12,9 @@ void PrintString(const char* str)
     printf("STR: %s\n", str);
 }
 
+void PrintBuffer(BufferData* buffer)
+{
+    printf("Data: %s. \nSIZE: %i\n", buffer->data, buffer->size);
+}
 
 #endif // TOML_PRINT_H
