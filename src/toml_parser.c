8680da0b3de87e9ab3e5fd82918d4a787a69664e
lukehayes <lukepurplehayes@gmail.com>
2024-07-19 09:43:39 +0100

    Added start of paring key value pairs

--- src/toml_parser.c
+++ src/toml_parser.c
@@ -1,23 +1,46 @@
 #include "toml_parser.h"
-#include "toml_print.h"
+// #include "toml_print.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+// #include <string.h>
 #include <ctype.h> // For isalpha() - Note to self.
 
 TomlToken* TokenizeToml(BufferData* buffer)
 {
-    // TODO Implement
     const char* data = buffer->data;
+    int bufSize = 100;
 
-    printf("Data? -> %c <- \n", *data);
-    printf("strchr? -> %i <- \n", strchr(*data, '['));
+    const char keyArr[bufSize];
+    const char valArr[bufSize];
 
-    // Check for the opening square bracket.
-    if(*data != '[')
+    while(*data != '\n')
     {
-        printf("TOML file doesn't start with a square bracket! Exiting.\n");
+        const char* key;
+        const char* val;
+
+
+        if(isalpha(*data))
+        {
+            printf("Alpha: %c \n", *data);
+
+            while(*data != isspace(*data))
+            {
+                data += 1;
+                printf("Non Space: %c \n", *data);
+            }
+
+
+            printf("DONE \n");
+        }
+
+
+
+        printf("Data %c\n", *data);
+        data += 1;
     }
+
+
+
 }
 
 TomlTable* ParseToml(BufferData* buffer)
@@ -44,6 +67,7 @@ void DestroyBufferData(BufferData* buffer)
     free(buffer->data);
     free(buffer);
 }
+
 int _CheckStartFormat(const char* data)
 {
     if(*data != '[')
